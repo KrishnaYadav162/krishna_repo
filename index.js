fb01235d7a0e3d8efb88d9b8c3fc564ed9ff99e5
KrishnaYadav162 <ky804366@gmail.com>
2025-03-25 15:51:06 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV')
+  res.send('krishna yadav')
 })
 
 app.get('/courses', function (req, res) {
